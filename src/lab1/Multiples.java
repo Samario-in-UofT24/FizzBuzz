bec4f466eec0f48ee4be5f4e278be482e8636edb
samario <samario.liu@mail.utoronto.ca>
2025-09-08 21:50:23 -0400

    View range and Reduce updated

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -8,7 +8,7 @@ public class Multiples {
         System.out.println(multiples(10, 3, 5));
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
@@ -18,7 +18,7 @@ public class Multiples {
         return count;
     }
 
-    static int multiples() {
+    public static int multiples() {
         return multiples(1000, 3, 5);
     }
 }
